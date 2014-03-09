6cd9a33feb2246f405c187f73ba4fc4a39d9fb8b
Bertrand Dunogier <bd@ez.no>
2014-03-09 04:13:57 +0100

    TMP: Added prefix to routes

---
+++ eZ/Publish/Core/REST/Client/HttpClient/Guzzle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * File containing the Guzzle class.
+ *
+ * @copyright Copyright (C) 2014 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+namespace eZ\Publish\Core\REST\Client\HttpClient;
+
+class Guzzle
+{
+
+}
